a58d69043d806f4e4f407e81a6df00095f95ec2d
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-27 04:18:34 -0800

    to check for a leaf in a tree

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -11,10 +11,10 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (!node)
 		return (0);
 
-	if (node->left == NULL)
+	if (node->left != NULL)
 		return (0);
 
-	if (node->left == NULL)
+	if (node->left != NULL)
 		return (0);
 
 	return (1);
