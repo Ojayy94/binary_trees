5fd51905365abbdabbf82dd96d6ee93b20c8e31a
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-27 14:21:21 -0800

    check for tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,5 @@
 #include "binary_trees.h"
 
-size_t binary_tree_height(const binary_tree_t *tree);
-
 /**
  * binary_tree_balance - A function that measures the balane factor
  * @tree: A pointer to the root node of the tree to measure it
