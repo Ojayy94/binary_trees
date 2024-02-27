ce6a1e179d823031de42597a4875c50249fe548c
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-27 04:02:05 -0800

    delete a tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - P2rogram to delete from a tree
+ * @tree: Tree to delete
+ * Return: Null if no trr
+ */
+
+void binary_tree_delete(binary_tree_t *tree);
+{
+	if (!tree)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
+}
