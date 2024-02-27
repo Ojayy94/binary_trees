944e2fb09a314c1759393167017ade30fd903cb5
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-27 04:46:33 -0800

    to traverse a tree

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/*
+ * binary_tree_preorder - Traverse a node
+ * @tree: Tree to check
+ * @func: Pointer to a function to call for each node
+ * Return: If tree or func is NULL, do nothing
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!(tree) || !(func))
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
