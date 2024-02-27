a46c185a1610b18d746bdaeba0e0d5bc0bfff649
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-27 14:23:16 -0800

    check for tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -21,3 +21,28 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (left_bal - right_bal);
 }
+
+/**
+ * binary_tree_height - A function that measures the height of a binary tree
+ * @tree: A pointer to the root node of the tree to measure the height
+ * Return: If tree or func is NULL, do nothing
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height);
+	return (right_height);
+}
