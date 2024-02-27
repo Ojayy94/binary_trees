23478fd48c14666a9ad176cbc5687d6fcb9c65ca
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-27 09:59:33 -0800

    count nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - A function that measures the size of a binary tree
+ * binary_tree_nodes - A function that measures the node of a binary tree
  * @tree: A pointer to the root node of the tree to measure the height
  * Return: If tree is NULL, return 0
  */
@@ -15,8 +15,12 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree->left || tree->right)
 		node = 1;
+	
+	if (tree->left)
+		node += binary_tree_nodes(tree->left);
 
-	else
-		return (node + binary_tree_nodes(tree.left) +
-			binary_tree_nodes(tree.right));
+	if (tree->right)
+		node += binary_tree_nodes(tree->right);
+
+	return (node);
 }
