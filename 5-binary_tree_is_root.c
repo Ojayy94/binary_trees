089fad2f7bc62cd88834d76662398ba92b2be98a
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-27 04:34:49 -0800

    to check for a root in a tree

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - To check for tree root
+ * @node: Node to check
+ * Return: 0 if NULL, 1 if leaf and 0 if otherwise
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node->parent)
+		return (1);
+
+	if (!node)
+		return (0);
+
+	return (0);
+}
