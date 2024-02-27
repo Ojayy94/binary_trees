d32c218e41a23fd0b153d4ad7df25b80f6cd91a6
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-27 06:15:57 -0800

    check tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (!tree)
+	if (tree)
 	{
 		left_height = binary_tree_height(root->left);
 		right_height = binary_tree_height(root->right);
