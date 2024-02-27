90b8c5d5985ca5aa30951b54c90c93220e1fbac1
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-27 09:09:15 -0800

    checking for depth of a tree

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Function that measures the depth of a node
+ * @tree: A pointer to the root node of the tree to measure the height
+ * Return: If tree or func is NULL, do nothing
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t tree_depth = 0;
+
+	tree_depth = 1 + binary_tree_depth(tree->parent);
+
+	if (!tree)
+		return (0);
+	
+	if (tree->parent)
+		return (tree_depth);
+}
