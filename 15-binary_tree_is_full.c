2e68d12b822384f8805ca48769f5af72aa6ca230
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-28 04:11:35 -0800

    full tree

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -16,7 +16,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 	if (tree->left && !(tree->right))
 		return (0);
 
-	if (!(tree->left) && tree->right)
+	if (tree->right && !(tree->left))
 		return (0);
 
 	if (tree->left)
