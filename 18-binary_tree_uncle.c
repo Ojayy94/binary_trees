3c5f4a0b54161f5ec69d080b4ede143a28042563
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-28 05:03:00 -0800

    check node uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		return (NULL);
 
 	if (node->parent->parent == NULL)
-                return (NULL);
+		return (NULL);
 
 	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
