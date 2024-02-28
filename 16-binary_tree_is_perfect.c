4d05b47d8a76051bdef09e26af89ba8dd957723b
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-28 04:48:35 -0800

    check node sibling

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_perfect - A function that checks if a binary tree is perfect
+ * @tree: A pointer to the root node of the tree to measure it
+ * Return: If tree is NULL, return 0
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if
+}
---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - A function that that finds the sibling of a node
+ * @node: A pointer to the sibling. Your function must return a pointer to the sibling node
+ * Return: If node is NULL or the parent is NULL, return NULL
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || parent == NULL)
+		return (NULL);
+
+	if (node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+
+	return (node->parent->left);
+}
