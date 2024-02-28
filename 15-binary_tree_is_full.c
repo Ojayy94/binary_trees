9c00152c78cc078ecaacd45f1f984686ed4fb924
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-28 03:59:54 -0800

    full tree

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full- A function that measures the height of a binary tree
+ * binary_tree_is_full - A function that measures the height of a binary tree
  * @tree: A pointer to the root node of the tree to check
  * Return: If tree is NULL, your function must return 0
  */
